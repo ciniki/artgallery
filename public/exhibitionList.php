5ab8070160be9a2ab71189fc169bd2aa2d76f0a5
Andrew Rivett <veggiefrog@gmail.com>
2013-04-25 02:57:43 -0400

    Resorted exhibition list

--- public/exhibitionList.php
+++ public/exhibitionList.php
@@ -52,7 +52,7 @@ function ciniki_artgallery_exhibitionList($ciniki) {
 		. "FROM ciniki_artgallery_exhibitions "
 		. "WHERE ciniki_artgallery_exhibitions.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
 		. "AND (webflags&0x01) = 0 "	// Visible on the website
-		. "ORDER BY start_date DESC, name";
+		. "ORDER BY ciniki_artgallery_exhibitions.start_date DESC, name";
 	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryTree');
 	$rc = ciniki_core_dbHashQueryTree($ciniki, $strsql, 'ciniki.artgallery', array(
 		array('container'=>'exhibitions', 'fname'=>'id', 'name'=>'exhibition',
